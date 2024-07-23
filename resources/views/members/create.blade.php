88a3f18489193af1461b3344f7918669d8b19abb
Arayik <ara.smbatyan@tumo.org>
2024-07-23 21:16:54 +0400

    Lesson 7 create member

--- resources/views/members/create.blade.php
+++ resources/views/members/create.blade.php
@@ -2,6 +2,61 @@
 
 @section('content')
     <div class="container">
-        Create
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <form action="{{route('member_store')}}" method="POST">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+            <div class="row">
+                <div class="col">
+                    <input type="text" name="first_name" class="form-control" placeholder="First name">
+                </div>
+                <div class="col">
+                    <input type="text" name="last_name" class="form-control" placeholder="Last name">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <input type="text" name="email" class="form-control" placeholder="Email">
+                </div>
+                <div class="col">
+                    <input type="text" name="image" class="form-control" placeholder="Image">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <input type="text" name="position" class="form-control" placeholder="Position">
+                </div>
+                <div class="col">
+                    <input type="text" name="joined_at" class="form-control" placeholder="Joined At">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <input type="number" name="sallary" class="form-control" placeholder="Salary">
+                </div>
+                <div class="col">
+                    <input type="integer" name="age" class="form-control" placeholder="Age">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <select class="form-control" name="gender" id="">
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                    </select>
+                </div>
+                <div class="col">
+                    <a href="{{route('member_index')}}" class="btn btn-sm btn-primary">Back</a>
+                    <input type="submit" class="btn btn-sm btn-success" value="Create Member">
+                </div>
+            </div>
+        </form>
     </div>
 @endsection
